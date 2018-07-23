d3896b8cd8b52940e9093e6d162f221f7a9df44d
kajal <kajal@krishaweb.net>
2018-07-23 16:44:48 +0530

    read me file

--- youcandothis/crud/src/commands/GenerateCrud.php
+++ youcandothis/crud/src/commands/GenerateCrud.php
@@ -37,21 +37,6 @@ class GenerateCrud extends Command
     protected function getOptions()
     {
         return [
-            ['fields', 'fi', InputOption::VALUE_OPTIONAL, 'An example option.', null],
-            ['all', 'a', InputOption::VALUE_NONE, 'Generate a migration, factory, and resource controller for the model'],
-
-            ['controller', 'c', InputOption::VALUE_NONE, 'Create a new controller for the model'],
-
-            ['factory', 'f', InputOption::VALUE_NONE, 'Create a new factory for the model'],
-
-            ['force', null, InputOption::VALUE_NONE, 'Create the class even if the model already exists.'],
-
-            ['migration', 'm', InputOption::VALUE_NONE, 'Create a new migration file for the model.'],
-
-            ['pivot', 'p', InputOption::VALUE_NONE, 'Indicates if the generated model should be a custom intermediate table model.'],
-
-            ['resource', 'r', InputOption::VALUE_NONE, 'Indicates if the generated controller should be a resource controller.'],
-
             ['views', 'vi', InputOption::VALUE_NONE, 'Create a views.'],
         ];
 
@@ -94,36 +79,6 @@ class GenerateCrud extends Command
         );
     }    
 
-    protected function createFactory()
-    {
-        $this->call('make:factory', [
-            'name' => $this->argument('name').'Factory',
-            '--model' => $this->argument('name'),
-        ]);
-    }
-
-    protected function createMigration()
-    {
-        $table = Str::plural(Str::snake(class_basename($this->argument('name'))));
-
-        $this->call('make:migration', [
-            'name' => "create_{$table}_table",
-            '--create' => $table,
-        ]);
-    }
-
-    protected function createController()
-    {
-        $controller = Str::studly(class_basename($this->argument('name')));
-
-        $modelName = $this->qualifyClass($this->getNameInput());
-
-        $this->call('make:controller', [
-            'name' => "{$controller}Controller",
-            '--model' => $this->option('resource') ? $modelName : null,
-        ]);
-    }
-
     protected function createViews()
     {
         $modelName = strtolower($this->argument('name'));
@@ -160,26 +115,6 @@ class GenerateCrud extends Command
 
     public function handle()
     {
-        if ($this->option('all')) {
-            $this->input->setOption('factory', true);
-            $this->input->setOption('migration', true);
-            $this->input->setOption('controller', true);
-            $this->input->setOption('resource', true);
-            $this->input->setOption('views', true);
-        }
-
-        if ($this->option('factory')) {
-            $this->createFactory();
-        }
-
-        if ($this->option('migration')) {
-            $this->createMigration();
-        }
-
-        if ($this->option('controller') || $this->option('resource')) {
-            $this->createController();
-        }
-
         if ($this->option('views')) {
             $this->createViews();
         }
